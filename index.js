ec3d67beac97f47afd97d76c9694fb844ef3af3f
EloiGry <eloi.grychta@gmail.com>
2021-10-24 15:46:48 +0200

    modif

--- index.js
+++ index.js
@@ -86,45 +86,145 @@
 
    
   
+  // function productFib(num){
 
-
-    function productFib(num){
-      var a = 0
-      var b = 1
    
-    while (a * b <= num) {
-      console.log(a)
-      console.log(b)
-      a = a + b
-      b = a + b
-    }
-    if (a * b === num) {
-      // console.log(a, b, true)
-    } else {
-      // console.log(a, b, false)
-    }
-
-  }
-  productFib(714)
-
-
-
-    
-    
-
-    // n =  
-    // F[n] * F[n+1] = prod
-
-    
-    
-    
-    
-    // var Fx2 = F.map(function(num) {
-    //   return num * num;
-    // });
+   
+  // let fib = [0,1]
   
-    
-    
+
+  //  while ((fib[fib.length -2]) * (fib[fib.length -1]) < num) {
+  //     let fibNumbers = (fib[fib.length -2]) +  (fib[fib.length -1])
+  //     fib = [...fib, fibNumbers]
+  //  }
+   
+
+  //  return [(fib[fib.length -2]), (fib[fib.length -1]), (fib[fib.length -2]) *  (fib[fib.length -1]) === num]
+
+  // }
+
+  // productFib(714)
+
+
+// -------------------------------------------------------------------------------------------------------------------
+  // function isPrime(num) {
+  //   let numbers = Array.from(Array(num).keys())
+  //   numbers.splice(0, 2)
+  //   let prime = true
+  //   numbers.forEach(number =>  {
+  //     if (num % number === 0 && num !== number) {
+  //       prime = false
+  //     }
+  //   })
+
+  //   return prime && num > 1
+  
+  // }
+  
+  // isPrime(25)
+
+// -------------------------------------------------------------------------------------------------------------------
+  
+  // function tribonacci(signature,n){
+  //   let array = [...signature] 
+  //   if (n === 0) {
+  //     return []
+  //   }
+
+  //   if (n === 1) {
+  //     return [(array[array.length -3])]
+  //   }
+
+  //   if (n === 2) {
+  //     return [(array[array.length -3]), (array[array.length -2]) ]
+  //   }
+  //   for (let i=3; i<n; i++) {
+  //     let tri = (array[array.length -3]) +  (array[array.length -2]) + (array[array.length -1])
+  //     array = [...array, tri]
+  //   }
+  //   return(array)
+  // }
 
    
-   
+  // tribonacci([1,1,1],10)
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// Write Number in Expanded Form
+
+// function expandedForm(num) {
+//   var index = 0
+//   var bracket = []
+//   var string = num.toString()
+//   var array = string.split("")
+//   var numbers = array.map(Number)
+//   var reverse = numbers.reverse()
+
+//   reverse.forEach(number => {
+//     var calc = number * 10**index
+//     index = index + 1
+//     if (calc !== 0) {
+//       bracket.push(calc)
+//     }
+//   });
+
+//   var result = bracket.reverse()
+//   var total = result.join(" + ")
+
+//   return (total)
+  
+//   }
+  
+  
+//   expandedForm(10342)
+
+// ------------------------------------------------------------------------------------
+  
+function bitsBattle(num) {
+  var arrayEven = []
+  var arrayOdd = []
+  var even = num.filter(even => even % 2 === 0 )
+  var odd = num.filter(odd => odd % 2 !== 0)
+  even.forEach(number => {
+    var bit = number.toString(2)
+    arrayEven.push(bit)
+  });
+  odd.forEach(number => {
+    var bit = number.toString(2)
+    arrayOdd.push(bit)
+  });
+
+  var joinEven = arrayEven.join("")
+  var joinOdd = arrayOdd.join("")
+
+  var countEven = 0
+  var countOdd = 0
+  for (var i=0; i< joinEven.length; i++) {
+    if (joinEven[i] === '0') {
+      countEven++
+    }
+  }
+
+  for (var i=0; i< joinOdd.length; i++) {
+    if (joinOdd[i] === '1') {
+      countOdd++
+    }
+}
+
+console.log(countEven)
+console.log(countOdd)
+
+if (countEven > countOdd) {
+  console.log ('evens wins')
+} else if (countOdd > countEven){
+  console.log ('odds wins')
+} else {
+  console.log ('tie')
+}
+
+}
+
+bitsBattle([3, 8, 22, 15, 78])
